13f6eaf488164cd09982e9597c100e1ca72442cd
Thiago <thberenger27@gmail.com>
2024-02-01 17:30:48 -0300

    att
    new cursor

--- script/sentence.js
+++ script/sentence.js
@@ -49,3 +49,26 @@ function selectPunishment(element) {
         element.style.borderLeft = ''
     }
 }
+
+
+// cursor ================
+
+const cursorDot = document.querySelector('.cursor-dot')
+const cursorOutline = document.querySelector('.cursor-outline')
+
+window.addEventListener('mousemove', function (e) {
+    const posX = e.clientX
+    const posY = e.clientY
+
+    cursorDot.style.left = `${posX}px`
+    cursorDot.style.top = `${posY}px`
+
+    // cursorOutline.style.left = `${posX}`
+    // cursorOutline.style.top = `${posY}`
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+    }, {duration: 500, fill: 'forwards'})
+
+})
